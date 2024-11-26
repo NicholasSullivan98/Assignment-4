acb9da75ea15b9e1cdcd2c269abc1f0f0e05b6e1
Nicholas Sullivan <sullnich@sheridancollege.ca>
2024-11-26 12:37:58 -0500

    Initial Commit

---
+++ HourlyWTrackerNicholas/HourlyWTrackerNicholasApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourlyWTrackerNicholasApp.swift
+//  HourlyWTrackerNicholas
+//
+//  Created by Nicholas Sullivan on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct HourlyWTrackerNicholasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
